e7960e9b732d0c1c70316978177dbb41036a1518
chenhanyu <chenhanyu987@gmail.com>
2025-11-07 17:47:00 +0800

    05-引入HttpRequest与HttpResponse

---
+++ src/main/java/server/HttpResponse.java
@@ -0,0 +1,174 @@
+package server;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
+import java.util.Locale;
+
+public class HttpResponse implements HttpServletResponse {
+    public String getCharacterEncoding() {
+        return null;
+    }
+    public String getContentType() {
+        return null;
+    }
+    public ServletOutputStream getOutputStream() throws IOException {
+        return null;
+    }
+    public PrintWriter getWriter() throws IOException {
+        return null;
+    }
+    public void setCharacterEncoding(String arg0) {
+    }
+    public void setContentLength(int arg0) {
+    }
+
+    @Override
+    public void setContentLengthLong(long len) {
+
+    }
+
+    public void setContentType(String arg0) {
+    }
+
+    @Override
+    public void setBufferSize(int size) {
+
+    }
+
+    @Override
+    public int getBufferSize() {
+        return 0;
+    }
+
+    @Override
+    public void flushBuffer() throws IOException {
+
+    }
+
+    @Override
+    public void resetBuffer() {
+
+    }
+
+    @Override
+    public boolean isCommitted() {
+        return false;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void setLocale(Locale loc) {
+
+    }
+
+    @Override
+    public Locale getLocale() {
+        return null;
+    }
+
+    public void addCookie(Cookie arg0) {
+    }
+
+    @Override
+    public boolean containsHeader(String name) {
+        return false;
+    }
+
+    @Override
+    public String encodeURL(String url) {
+        return "";
+    }
+
+    @Override
+    public String encodeRedirectURL(String url) {
+        return "";
+    }
+
+    @Override
+    public String encodeUrl(String url) {
+        return "";
+    }
+
+    @Override
+    public String encodeRedirectUrl(String url) {
+        return "";
+    }
+
+    @Override
+    public void sendError(int sc, String msg) throws IOException {
+
+    }
+
+    @Override
+    public void sendError(int sc) throws IOException {
+
+    }
+
+    @Override
+    public void sendRedirect(String location) throws IOException {
+
+    }
+
+    @Override
+    public void setDateHeader(String name, long date) {
+
+    }
+
+    @Override
+    public void addDateHeader(String name, long date) {
+
+    }
+
+    @Override
+    public void setHeader(String name, String value) {
+
+    }
+
+    @Override
+    public void addHeader(String name, String value) {
+
+    }
+
+    @Override
+    public void setIntHeader(String name, int value) {
+
+    }
+
+    @Override
+    public void addIntHeader(String name, int value) {
+
+    }
+
+    @Override
+    public void setStatus(int sc) {
+
+    }
+
+    @Override
+    public void setStatus(int sc, String sm) {
+
+    }
+
+    @Override
+    public int getStatus() {
+        return 0;
+    }
+
+    public String getHeader(String arg0) {
+        return null;
+    }
+    public Collection<String> getHeaderNames() {
+        return null;
+    }
+    public Collection<String> getHeaders(String arg0) {
+        return null;
+    }
+}
